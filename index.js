b546f72c5990807db36950e4f0f2ba3c3d7e1399
kamal <kamal.kant@aptean.com>
2020-01-25 20:15:25 +0530

    Remove extra line space from file

--- index.js
+++ index.js
@@ -8,44 +8,40 @@ let statusCode, message;
 let totalRows;
 
 exports.handler = async (event, context) => {
+	var queryResponse = await database.query(process.env.QUERY)
+		.then(rows => {
+			totalRows = rows;
+			return database.close();
+		})
+		.then(async () => {
+			// do something with someRows and otherRows
+			return totalRows;
+		}, async err => {
+			return database.close().then(() => { throw err; })
+		})
+		.then((resultRows) => {
+			return { statusCode: 200, result: resultRows, message: "Working as expected", };
+		})
+		.catch(err => {
+			return { statusCode: 501, errorMessage: JSON.parse(JSON.stringify(err)).sqlMessage, message: "Query isn't executing." };
+		});
+	
+	let excelObj = new eObj(headerJsonObj);
 
-		var queryResponse = await database.query(process.env.QUERY)
-			.then(rows => {
-				totalRows = rows;
-				return database.close();
-			})
-			.then(async () => {
-				// do something with someRows and otherRows
-				return totalRows;
-			}, async err => {
-				return database.close().then(() => { throw err; })
-			})
-			.then((resultRows) => {
-				return { statusCode: 200, result: resultRows, message: "Working as expected", };
-			})
-			.catch(err => {
-				return { statusCode: 501, errorMessage: JSON.parse(JSON.stringify(err)).sqlMessage, message: "Query isn't executing." };
-			});
-		
-		let excelObj = new eObj(headerJsonObj);
+	if(queryResponse.statusCode == 200 && queryResponse.result.length > 0) {
+		statusCode =  queryResponse.statusCode;
+		message =  queryResponse.message;
+		await excelObj.writeFileToS3Bucket(queryResponse.result).then((res) => {
+			console.log(res);	
+		})
+	}else{
+		statusCode =  queryResponse.statusCode;
+		message =  queryResponse.message;
+	}
 
-		if(queryResponse.statusCode == 200 && queryResponse.result.length > 0) {
-			
-			statusCode =  queryResponse.statusCode;
-			message =  queryResponse.message;
-
-			await excelObj.writeFileToS3Bucket(queryResponse.result).then((res) => {
-				console.log(res);	
-			})
-
-		}else{
-			statusCode =  queryResponse.statusCode;
-			message =  queryResponse.message;
-		}
-
-		return {
-			statusCode:statusCode,
-			message:message
-		}
+	return {
+		statusCode:statusCode,
+		message:message
+	}
 }
 
