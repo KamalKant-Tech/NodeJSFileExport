415ac7bde9388ae922ea0e0e8fd961798deaf8b8
kamal <kamal.kant@aptean.com>
2020-01-27 17:38:42 +0530

    Add dynamic column value extration like date & phone

--- testExcel.js
+++ testExcel.js
@@ -22,10 +22,9 @@ module.exports = class ExcelExport {
     });
   }
 
-  writeFileToS3Bucket(snapData) {
+  writeFileToS3Bucket(data) {
     // create a workbook variable
     this.workbook = new Excel.Workbook();
-    
     // read excel file from the path
     /* workbook.xlsx.readFile(excel_file_path)
       .then(function() {
@@ -37,13 +36,15 @@ module.exports = class ExcelExport {
 
     this.worksheet = this.workbook.addWorksheet('My Sheet');
     this.worksheet = this.workbook.getWorksheet('My Sheet');
-    return this.addRowsToExcelSheet(snapData,this.workbook,this.worksheet).then((result)=> {return result});
+    return this.addRowsToExcelSheet(data,this.workbook,this.worksheet).then((result)=> {return result});
   }
 
   addColumnsToExcel() {
     for (const key in this.headerObj) {
       if (this.headerObj.hasOwnProperty(key)) {
-        headerArray.push({header : this.headerObj[key],key: this.headerObj[key] , width : 10,outlineLevel: 1});
+        if(typeof(this.headerObj[key]) !== 'object') {
+          headerArray.push({header : this.headerObj[key],key: this.headerObj[key] , width : 10,outlineLevel: 1});
+        } 
       }
     }
     return headerArray;
@@ -53,39 +54,86 @@ module.exports = class ExcelExport {
       return (isNaN(date) && new Date(date) !== "Invalid Date" && !isNaN(new Date(date)) ) ? true : false;
   }
 
+  //Format Excel Phone Column
+  formatPhone(phoneNumber) {
+		// Split into phone number and extension. The system doesn't currently store extensions with the phone number (as of Jan 2018). This is for future usage.
+    var phone,extension= '';
+    
+		let phone_parts = phoneNumber.split('x');
+
+		if (typeof phone_parts[0] !== undefined) {
+			phone = phone_parts[0].length != 0 ? phone_parts[0] : '';
+		}
+    
+		if (typeof phone_parts[0] !== undefined) {
+			extension = phone_parts[1] ? phone_parts[1] : '';
+		}
+
+    // Regular express [^0-9] looks for characters that are not a number.
+    var phonetemp =  phone.replace("/[^0-9]/",'');
+    if (phonetemp.length == 10 && phonetemp[0] != '0') {
+			phone = '(' + phonetemp.substr(0, 3) + ') ' + phonetemp.substr(3, 3) + '-' + phonetemp.substr(6, 4);
+		} else if (phonetemp.length == 10 && phonetemp[0] == '0') {
+			// Australia
+			if (phone.substr(0, 2) == '04') {
+				// Mobile 04xx xxx xxx
+				phone = phonetemp.substr(0, 4) + ' ' + phonetemp.substr(4, 3) + ' ' + phonetemp.substr(7, 3);
+			} else {
+				// Landline (0x) xxxx xxxx
+				phone = '(' + phonetemp.substr(0, 2) + ') ' + phonetemp.substr(2, 4) + ' ' + phonetemp.substr(6, 4);
+			}
+		}
+		//phone = htmlspecialchars(phone);
+		// Add the extension back in
+		if (extension.length != 0) {
+			phone = ' x' . extension;
+		}
+		return phone;
+
+	}
+
   // code to write data into excel sheet
-  addRowsToExcelSheet(snapData,workbook,worksheet){
-      let parentThis = this;
+  addRowsToExcelSheet(data,workbook,worksheet){
       worksheet.columns = this.addColumnsToExcel();
-      
       // for loop to read each record from query response
-      snapData.forEach(function(val,index){
+      data.forEach(function(val,index){
         var rowValues = [];
-        Object.keys(parentThis.headerObj).forEach((headerVal) => {
+        let customElementConfigurationStatus = this.headerObj.hasOwnProperty('customElementConfiguration');
+        Object.keys(this.headerObj).forEach((headerVal) => {
+          
           if(!val.hasOwnProperty(headerVal)) {
             let headerSplit = headerVal.split('-');
             val[headerVal] = (headerSplit.length > 1 && val[headerSplit[0]] != null ) ? parser.parse(val[headerSplit[0]])[headerSplit[1]] : ''
           }
-
-          if(parentThis.isDate(val[headerVal])) {
-            val[headerVal] = dateFormat(val[headerVal], "m/d/yyyy") ; 
+          
+          if(customElementConfigurationStatus && this.headerObj.customElementConfiguration.hasOwnProperty(headerVal)) {
+            let eleType = this.headerObj.customElementConfiguration[headerVal];
+            if(this.isDate(val[headerVal]) && eleType != 'phone') {
+              val[headerVal] = dateFormat(val[headerVal],eleType) ;
+            }else if(eleType == 'phone'){
+              val[headerVal] = val[headerVal].length != 0 ? this.formatPhone(val[headerVal]) : '';
+            }
           }
-
           rowValues.push(val[headerVal])
-        })
+        },this)
       
         worksheet.addRow(rowValues);
-      })
+      },this)
 
       //write file function to save all the data to the excel template file into S3 bucket.
       //workbook.xlsx.write(stream)
-      return workbook.xlsx.write(stream).then(() => {
-        return this.createS3Client().upload({
+      return workbook.xlsx.writeFile('sample.xlsx').then(() => {
+        /* return this.createS3Client().upload({
             Key: fileName,
             Bucket: process.env.BUCKET_NAME,
             Body: stream,
             ContentType: 'application/vnd.ms-excel'
-        }).promise();
+        }).promise(); */
+        return {
+          Key: fileName,
+          Bucket: process.env.BUCKET_NAME,
+          ContentType: 'application/vnd.ms-excel'
+        }
       })
       .then((res) => {
         return res;
